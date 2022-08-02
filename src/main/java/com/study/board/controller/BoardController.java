e40aa284cd6ef57e85bdb02394aa03d8c3f547e2
msj0923 <110479613+msj0923@users.noreply.github.com>
2022-08-03 00:32:54 +0900

    Add files via upload

---
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -0,0 +1,109 @@
+package com.study.board.controller;
+
+
+import com.study.board.entity.Board;
+import com.study.board.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@Controller
+public class BoardController {
+
+    @Autowired
+    private BoardService boardService;
+
+    @GetMapping("/board/main") //localhost:8080/board/write
+    public String boardWriteForm(){
+
+        return "board/boardmain";
+
+    }
+
+    @PostMapping("/board/writepro")
+    public String boardWritePro(Board board, Model model, MultipartFile file) throws Exception{
+
+        boardService.write(board, file);
+        //if문으로 실패작성하기!!
+        model.addAttribute("message","글 작성이 완료되었습니다.");
+//      model.addAttribute("message","글 작성이 실패하였습니다.");
+        model.addAttribute("searchUrl","/board/list");
+
+
+
+        return "board/message";
+    }
+
+    @GetMapping("/board/list")
+    public String boardList(Model model,
+                            @PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.DESC) Pageable pageable,
+                            String searchKeyword) {
+
+        Page<Board> list = null;
+
+        if (searchKeyword == null){
+             list = boardService.boardList(pageable);
+        }
+        else{
+             list = boardService.boardSearchList(searchKeyword, pageable);
+        }
+
+        int nowPage = list.getPageable().getPageNumber() + 1;
+        int startPage = Math.max(nowPage -4, 1);
+        int endPage = Math.min(nowPage + 5, list.getTotalPages());
+
+        model.addAttribute("list", list);
+        model.addAttribute("nowPage", nowPage);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+
+        return "board/boardlist";
+    }
+
+    @GetMapping("/board/view") //localhost:8080/board/view?id=1
+    public String boardView(Model model, Integer id){
+
+        model.addAttribute("board",boardService.boardView(id));
+
+        return "board/boardview";
+    }
+
+    @GetMapping("/board/delete")
+    public String boardDelete(Integer id){
+
+        boardService.boardDelete(id);
+
+        return "redirect:/board/list";
+    }
+
+    @GetMapping("/board/modify/{id}")
+    public String boardModify(@PathVariable("id")Integer id, Model model){
+
+        model.addAttribute("board",boardService.boardView(id));
+
+        return "board/boardmodify";
+    }
+
+    @PostMapping("/board/update/{id}")
+    public String boardUpdate(@PathVariable("id") Integer id, Board board, MultipartFile file) throws Exception {
+
+        Board boardTemp = boardService.boardView(id);
+        boardTemp.setTitle(board.getTitle());
+        boardTemp.setContent(board.getContent());
+
+        boardService.boardModify(boardTemp, file);
+
+        return "redirect:/board/list";
+    }
+}
